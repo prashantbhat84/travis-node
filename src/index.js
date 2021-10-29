5b5a2a59da2e7fcdc6960038228b1c4a9f7bba08
Prashant Bhat <prashantbhat84@gmail.com>
2021-10-29 20:31:45 +0530

    travis aws deploy

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const express     = require('express')
-require('./db/mongoose');
+// require('./db/mongoose');
 const userRoutes = require('./router/user')
 const PostRoutes = require('./router/post')
 
